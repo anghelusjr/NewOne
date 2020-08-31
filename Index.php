a40d66b0a37e688c2bbdcd98e980dae08e38f83b
Angel Buenavista <anghelusjr09.ab@gmail.com>
2020-08-31 23:27:00 +0800

    Initial commit

---
+++ Index.php
@@ -0,0 +1,43 @@
+<html>
+    <head>
+        <title>Index</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="MyCss.css">
+
+    </head>
+
+    <body> 
+        <div class="banner-text">
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Portfolio</li>
+                <li>Services</li>
+                <li>Contact</li>
+            </ul>
+
+            <h1>welcome</h1>
+        </div>  
+
+        <div class="animation-area">
+            <ul class="box-area">
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+
+            </ul>
+            
+
+        </div>
+
+        <div class="about">
+
+
+        </div>
+    </body>
+
+</html>
